12717f6391cdc2f839b5c07990c99bfebf190560
smrft <smrft@smrft.org>
2023-05-26 14:19:45 +0530

    deploy

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -161,17 +161,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # for authentication
 
 AUTH_USER_MODEL = 'AttendanceApp.Admin'
-CORS_ALLOWED_ORIGINS = [
-    'https://smrftadmin.netlify.app/',  # Add the URL of your React app
-    # Add any other allowed origins if needed
-]
+
 APPEND_SLASH = False
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = [
+    'https://smrftadmin.netlify.app',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://smrftadmin.netlify.app',
+]
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = (
-  'https://smrftadmin.netlify.app/',
-)
+
 CORS_ALLOW_METHODS = [
 'DELETE',
 'GET',
