f693eeecaf86e94d117ac2bfb2e0fd9fe8a431e2
smrft <smrft@smrft.org>
2023-05-26 13:37:21 +0530

    deploy

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -169,10 +169,28 @@ APPEND_SLASH = False
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = (
   'https://smrftadmin.netlify.app/',
 )
-
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 # mongo storage connection
 
 GRIDFS_STORAGE_OPTIONS = {
