3206b4fb49fe05cd484ae5202f92de9383d5429e
smrft <smrft@smrft.org>
2023-05-29 14:34:58 +0530

    deploy

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -51,9 +51,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'https://smrftadmin.netlify.app',
-] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOW_ALL_ORIGINS = True
+ # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'https://smrftadmin.netlify.app',
 ]
