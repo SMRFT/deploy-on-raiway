131c94c464acf2d68b13c0004e9e3aa5919e1b17
smrft <smrft@smrft.org>
2023-06-09 10:10:43 +0530

    changes on settings

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -217,10 +217,3 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
-# Static and Media File Settings
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
