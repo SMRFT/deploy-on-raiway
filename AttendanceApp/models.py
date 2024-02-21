7ebe9ec288939ba340804637caa636fa72acfb05
SMRFT <110091550+SMRFT@users.noreply.github.com>
2024-02-21 11:13:41 +0530

    Update models.py

--- AttendanceApp/models.py
+++ AttendanceApp/models.py
@@ -13,16 +13,7 @@ from django.http import JsonResponse
 from gridfs_storage.storage import GridFSStorage
 from django.utils import timezone
 
-
-
-import datetime
-import uuid
-
-
-
-# Employee
-
-gridfs_storage = GridFSStorage()
+# Employee model (Add employee model)
 class Employee(models.Model):
     id = models.CharField(primary_key=True, max_length=500)
     name = models.CharField(max_length=500)
@@ -60,33 +51,48 @@ class Employee(models.Model):
     companyEmail=models.CharField(max_length=500,blank=True, null=True)
     assetDetails=models.CharField(max_length=1000,blank=True, null=True)
     reportedBy=models.CharField(max_length=1000,blank=True, null=True)
+    
 
-
+# Employee model (deleted employee model)
 class DeletedEmployee(models.Model):
     id = models.CharField(primary_key=True, max_length=500)
     name = models.CharField(max_length=500)
-    email = models.CharField(max_length=500)
+    Gender = models.CharField(max_length=10)
+    dob = models.DateField(null=True)
+    age = models.CharField(max_length=500,blank=True)
+    Maritalstatus = models.CharField(max_length=10,blank=True)
     mobile = models.CharField(max_length=500)
     department = models.CharField(max_length=500)
-    deleted_at = models.DateField()
+    RNRNO = models.CharField(max_length=1000,blank=True, null=True)
+    TNMCNO = models.CharField(max_length=500, blank=True, null=True)
+    ValidlityDate = models.DateField(null=True)
+    email = models.CharField(max_length=500,blank=True)
+    dateofjoining = models.DateField(null=True)
+    bankaccnum = models.CharField(max_length=500,blank=True)
     designation = models.CharField(max_length=500)
+    Aadhaarno = models.CharField(max_length=500)
+    PanNo = models.CharField(max_length=500,blank=True)
+    BloodGroup = models.CharField(max_length=500,blank=True)
     address = models.CharField(max_length=500)
     educationData = models.CharField(max_length=1200)
     experienceData = models.CharField(max_length=1200)
     referenceData = models.CharField(max_length=1200)
-    languages = models.CharField(max_length=500, blank=True)
-    Aadhaarno = models.CharField(max_length=500)
-    PanNo = models.CharField(max_length=500, blank=True, null=True)
-    BloodGroup = models.CharField(max_length=500)
-    RNRNO = models.IntegerField(blank=True, null=True, default=None)
-    TNMCNO = models.CharField(max_length=500, blank=True, null=True)
-    ValidlityDate = models.DateField(blank=True, null=True)
-    dateofjoining = models.CharField(max_length=500, blank=True, null=True)
-    bankaccnum = models.IntegerField(blank=True, null=True)
-    salary = models.IntegerField(blank=True, null=True)
-
-
-# Admin Login
+    languages = models.CharField(max_length=1200,blank=True)
+    salary = models.CharField(max_length=1000,blank=True, null=True)
+    PF = models.CharField(max_length=1200,blank=True)
+    ESINO = models.CharField(max_length=1200,blank=True)
+    employmentCategory=models.CharField(max_length=500)
+    employeeType=models.CharField(max_length=500)
+    medicalClaimPolicyNo=models.CharField(max_length=500 ,null=True)
+    validityDateFrom=models.DateField(null=True)
+    validityDateTo=models.DateField(null=True)
+    bankName=models.CharField(max_length=500)
+    ifscCode=models.CharField(max_length=500)
+    companyEmail=models.CharField(max_length=500,blank=True, null=True)
+    assetDetails=models.CharField(max_length=1000,blank=True, null=True)
+    reportedBy=models.CharField(max_length=1000,blank=True, null=True)
+    deleted_at=models.CharField(max_length=500)
+    
 from bson import ObjectId
 class ObjectIdField(models.CharField):
     def __init__(self, *args, **kwargs):
@@ -95,7 +101,6 @@ class ObjectIdField(models.CharField):
         kwargs['editable'] = False
         kwargs['default'] = str(ObjectId())
         super(ObjectIdField, self).__init__(*args, **kwargs)
-    
     def pre_save(self, model_instance, add):
         if add and not getattr(model_instance, self.attname):
             setattr(model_instance, self.attname, str(ObjectId()))
@@ -107,55 +112,29 @@ class Admin(AbstractUser):
     password = models.CharField(max_length=500)
     role = models.CharField(max_length=100,)
     mobile = models.CharField(max_length=100, blank=True, null=True)
-
-    
     # You can specify 'id' explicitly
     id = ObjectIdField()
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    def __str__(self):
-        return self.email
 
-class PasswordResetRequest(models.Model):
-    email = models.EmailField()
-    reset_code = models.CharField(max_length=6)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    is_used = models.BooleanField(default=False)
-
-class Designation(models.Model):
-    label = models.CharField(max_length=500)
-    value = models.IntegerField()
 
 # department count details model (Donut chart)
-
 class department(models.Model):
     label = models.CharField(max_length=500)
     value = models.IntegerField()
-
-
+    
 
 # Employee model by department (Donut chart)
-
 class Employeebydepartment(models.Model):
     id = models.CharField(max_length=500,primary_key=True)
     name = models.CharField(max_length=500)
     mobile = models.CharField(max_length=500)
     department = models.CharField(max_length=500)
     address = models.CharField(max_length=500)
-# Employee model by Designation
 
-
-class Employeebydesignation(models.Model):
-    id = models.CharField(max_length=500)
-    name = models.CharField(max_length=500)
-    mobile = models.CharField(max_length=500)
-    designation = models.CharField(max_length=500, primary_key=True)
-    address = models.CharField(max_length=500)
 
 # Calendar Model
-
-
 class Admincalendarlogin(models.Model):
     id = models.CharField(max_length=500)
     iddate = models.CharField(max_length=500, primary_key=True)
@@ -171,9 +150,23 @@ class Admincalendarlogin(models.Model):
     latelogin = models.TimeField()
     earlyLogout = models.TimeField()
 
+class Calendarlogin(models.Model):
+    id = models.CharField(max_length=500)
+    iddate = models.CharField(max_length=500, primary_key=True)
+    email = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    start = models.DateTimeField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    shift = models.CharField(max_length=500)
+    date = models.DateField()
+    day = models.IntegerField()
+    leavetype = models.CharField(max_length=500)
+    latelogin = models.TimeField()
+    earlyLogout = models.TimeField()
+
+
 # Event calendar model
-
-
 class Hour(models.Model):
     id = models.CharField(max_length=500, primary_key=True)
     name = models.CharField(max_length=500)
@@ -184,10 +177,9 @@ class Hour(models.Model):
     barColor = models.CharField(max_length=500)
     text = models.CharField(max_length=500)
     leavetype = models.CharField(max_length=500)
-    
+
+
 # Summary calendar model
-
-
 class Summary(models.Model):
     id = models.CharField(max_length=500)
     name = models.CharField(max_length=500)
@@ -200,16 +192,14 @@ class Summary(models.Model):
     overtimehours = models.CharField(max_length=500)
     workedhours = models.CharField(max_length=500)
 
+
 # Employee calendar export model for single employee
-
-
-
-
 class Employeeexport(models.Model):
     id = models.CharField(max_length=500, primary_key=True)
     name = models.CharField(max_length=500)
     date = models.DateField()
     month = models.IntegerField()
+    year = models.IntegerField()
     start = models.DateTimeField()
     end = models.DateTimeField()
     shift = models.CharField(max_length=500)
@@ -217,10 +207,11 @@ class Employeeexport(models.Model):
     Breakhour = models.CharField(max_length=500)
     overtimehours = models.CharField(max_length=500)
     Total_hours_worked = models.CharField(max_length=500)
+    Total_overtime_hrs = models.CharField(max_length=500)
     leavetype = models.CharField(max_length=500)
+
+
 # Employee calendar export model for all the employee
-
-
 class Summaryexport(models.Model):
     id = models.CharField(max_length=500, primary_key=True)
     name = models.CharField(max_length=500)
@@ -235,6 +226,10 @@ class Summaryexport(models.Model):
     loss_of_pay = models.IntegerField()
     total_weekoff = models.IntegerField()
     weekoff_used = models.IntegerField()
+    remaining_weekoff = models.IntegerField()
+    Days_in_a_month = models.IntegerField()
+    payroll = models.IntegerField()
+    Total_overtime_hrs = models.CharField(max_length=500)
 
 
 class Breakhours(models.Model):
@@ -263,13 +258,27 @@ class EmployeeHours(models.Model):
     designation = models.CharField(max_length=500)
 
 
+class PasswordResetRequest(models.Model):
+    email = models.EmailField(primary_key=True)
+    reset_code = models.CharField(max_length=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
 class UserPermission(models.Model):
     role = models.CharField(max_length=100)
     employee = models.BooleanField(default=False)
     add_employee = models.BooleanField(default=False)
     dashboard = models.BooleanField(default=False)
     pending_approval = models.BooleanField(default=False)
-    admin_registration = models.BooleanField(default=False)
+    events = models.BooleanField(default=False)
+    employee_exit_form = models.BooleanField(default=False)
+
+class LeaveRequest(models.Model):
+    mail = models.CharField(max_length=500, primary_key=True)
+    start_date = models.CharField(max_length=500)
+    end_date = models.CharField(max_length=500, blank=True, null=True)
+    leave_type = models.CharField(max_length=500, blank=True, null=True)
+    reason = models.CharField(max_length=500, blank=True, null=True)
 
 class EmployeeExitForm(models.Model):
     name = models.CharField(max_length=500)
@@ -277,10 +286,6 @@ class EmployeeExitForm(models.Model):
     department = models.CharField(max_length=500)
     exitStatus = models.CharField(max_length=500)
     lastWorkingDate = models.CharField(max_length=500)
-
-
-
-
 
 class AWSCredentials(models.Model):
     access_key_id = models.CharField(max_length=255)
